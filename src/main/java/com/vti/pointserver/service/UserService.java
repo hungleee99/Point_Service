fcf03c5d51c71061c301b74f75c5f182ed18b5b1
avganti1102 <58077430+avganti1102@users.noreply.github.com>
2021-01-19 12:03:37 +0700

    update

--- src/main/java/com/vti/pointserver/service/UserService.java
+++ src/main/java/com/vti/pointserver/service/UserService.java
@@ -11,7 +11,9 @@ import com.vti.pointserver.entity.User;
 public interface UserService extends UserDetailsService {
 
 	List<User> getAllUsers();
-	public User getByrole(String role);
+//	public User getByrole(String role);
+	List<User> getByrole(String role);
+	public User getByid(int id);
 	public void createUser(User user);
 	public void updateUser(User user);
 	public void deleteUser(int id);
