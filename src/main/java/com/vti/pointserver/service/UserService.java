af9e646e93c111a805fba7ab03da594c2ba9ffb8
hunglee <hungxuanne1999@gmail.com>
2021-01-19 22:55:36 +0700

    completed task

--- src/main/java/com/vti/pointserver/service/UserService.java
+++ src/main/java/com/vti/pointserver/service/UserService.java
@@ -14,10 +14,13 @@ import com.vti.pointserver.entity.User;
 
 public interface UserService extends UserDetailsService {
 	List<User> getAllUsers();
-//	public User getByrole(String role);
 	List<User> getByrole(String role);
-	public User getByid(int id);
+	List<User> getByName(String name);
+	List<User> findUserInGroup(String role,String name);
+	User getById(int id);
 	public void createUser(User user);
 	public void updateUser(User user);
 	public void deleteUser(int id);
+	public void deletebyrole(String role);
+	
 }
